4bd7c2c2626bb363f170e1b4128803b89d1a6343
Daniel Betz <daniel.betz@jambit.com>
2020-02-27 17:12:41 +0100

    cloud-init for kube creation

--- main.tf
+++ main.tf
@@ -6,12 +6,28 @@ data "hcloud_network" "kubernetes" {
   name = "kubenet"
 }
 
+data "hcloud_floating_ip" "kubernetes" {
+  name = "kubernetes-loadbalancer"
+}
+
+data "template_file" "cloud_init_master" {
+  template = file("templates/cloud-init-master.tpl")
+
+  vars = {
+    floating_ip = data.hcloud_floating_ip.kubernetes.ip_address
+  }
+}
+
+output "bla" {
+  value = data.template_file.cloud_init_master.rendered
+}
+
 resource "hcloud_server" "kubernetes_master" {
   name = "${var.kubernetes_prefix}-master"
   image = var.kubernetes_image
   location = var.location
   server_type = var.kubernetes_master_server_type
-  user_data = file("templates/cloud-init-master.tpl")
+  user_data = data.template_file.cloud_init_master.rendered
   ssh_keys = [var.ssh_key_name]
 }
 
@@ -20,4 +36,3 @@ resource "hcloud_server_network" "kubernetes_master" {
   network_id = data.hcloud_network.kubernetes.id
   ip = var.kubernetes_master_internal_ip
 }
-
